1144f1f7a8496727290980b1abf057a0f36c49d5
seonwoo0808 <me@pentag.kr>
2024-12-19 23:05:56 +0900

    add edge runtime setup

--- src/app/post/route.ts
+++ src/app/post/route.ts
@@ -2,6 +2,8 @@ import { redirect } from 'next/navigation';
 
 import { AnnouncementPageURL } from '@/constant';
 
+export const runtime = 'edge';
+
 export async function GET() {
   redirect(AnnouncementPageURL);
 }
